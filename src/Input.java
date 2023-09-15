7e9d33e949d7641428352c5e0f3c62cad2d599cd
Harrison Alexander <benjamin.alexander7168@gmail.com>
2023-09-15 10:01:26 -0500

    Final

--- src/Input.java
+++ src/Input.java
@@ -4,6 +4,8 @@ public class Input {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         int intOne, intTwo;
+        double doubleOne ;
+        float floatOne ;
 
         System.out.print("Enter integer one :: ");
         intOne = keyboard.nextInt();
@@ -13,6 +15,14 @@ public class Input {
 
         System.out.println("integer one = " + intOne);
         System.out.println("integer two = " + intTwo);
+
+        System.out.print("Enter double one :: ");
+        doubleOne = keyboard.nextInt();
+        System.out.print("Enter float one :: ");
+        floatOne = keyboard.nextFloat();
+
+        System.out.print("float one = " + floatOne);
+        System.out.print("double one =" + doubleOne);
 
         //add more code here for the other variables and data types
         }
