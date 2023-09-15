cd592bb522eaaf5243d5d4e08acaaf131862bc62
Harrison Alexander <benjamin.alexander7168@gmail.com>
2023-09-15 09:58:12 -0500

    Final

--- src/Input.java
+++ src/Input.java
@@ -1,5 +1,19 @@
-public class Main {
+import java.util.Scanner;
+
+public class Input {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner keyboard = new Scanner(System.in);
+        int intOne, intTwo;
+
+        System.out.print("Enter integer one :: ");
+        intOne = keyboard.nextInt();
+
+        System.out.print("Enter integer two :: ");
+        intTwo = keyboard.nextInt();
+
+        System.out.println("integer one = " + intOne);
+        System.out.println("integer two = " + intTwo);
+
+        //add more code here for the other variables and data types
+        }
     }
-}
